ce5dfae2af2d8b71d46047c69a4ffc13e9c04a6b
unknown <avanidhalgara01@gmail.com>
2020-09-08 02:01:43 -0500

    created navbar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
+import {Layout, Navigation, Header, Content, Drawer} from 'react-mdl'
+import Main  from './components/main'
+import { Link } from 'react-router-dom'
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="demo-big-content">
+    <Layout>
+        <Header title="Title" scroll>
+            <Navigation>
+                <Link to="/about">About Me</Link>
+                <Link to="/resume">Resume</Link>
+                <Link to="/projects">Project</Link>
+                <Link to="/contact">Contact</Link>
+            </Navigation>
+        </Header>
+        <Drawer title="Title">
+            <Navigation>
+              <Link to="/about">About Me</Link>
+                <Link to="/resume">Resume</Link>
+                <Link to="/projects">Project</Link>
+                <Link to="/contact">Contact</Link>
+            </Navigation>
+        </Drawer>
+        <Content>
+            <div className="page-content" />
+        <Main/>
+        </Content>
+    </Layout>
+</div>
     </div>
   );
 }
---
+++ src/components/main.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import LandingPage from './landingpage'
+import About from './about'
+import Contact from './contact'
+import Resume from './resume'
+import Projects from './projects'
+
+
+
+
+const Main =() =>(
+    <Switch>
+        <Route exact path ='/' component={LandingPage} />
+        <Route  path ='/about' component={About} />
+        <Route path ='/contact' component={Contact} />
+        <Route path ='/resume' component={Resume} />
+        <Route path ='/projects' component={Projects} />
+        
+
+
+    </Switch>
+)
+export default Main
