11de16e7a14c36a7133c89e42684142bf3f464f9
unknown <avanidhalgara01@gmail.com>
2020-09-15 15:06:28 -0500

    Updates

---
+++ precache-manifest.2e596fbc9f6e3b9308cf8d415e068692.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f5d592987aedb2baaa2776fc8c11330",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "cd6d7296fbeb7e45954f",
+    "url": "/react-portfolio/static/css/2.cd1c2086.chunk.css"
+  },
+  {
+    "revision": "26755617eacb74219ae0",
+    "url": "/react-portfolio/static/css/main.c343a9bf.chunk.css"
+  },
+  {
+    "revision": "cd6d7296fbeb7e45954f",
+    "url": "/react-portfolio/static/js/2.8bdc9a18.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/react-portfolio/static/js/2.8bdc9a18.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26755617eacb74219ae0",
+    "url": "/react-portfolio/static/js/main.a557d384.chunk.js"
+  },
+  {
+    "revision": "4041639520ba55d92bf7",
+    "url": "/react-portfolio/static/js/runtime-main.0f5f5400.js"
+  }
+]);
