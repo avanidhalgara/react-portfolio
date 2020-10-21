3953b737f94dbffacb66c19eff968f23664f8843
unknown <avanidhalgara01@gmail.com>
2020-10-21 14:27:03 -0500

    updated about me

--- src/components/about.js
+++ src/components/about.js
@@ -15,9 +15,9 @@ class About extends Component{
                <Cell col={12}>
                <h3 style={{color:'white'}}>About Me</h3>
                   <hr/>                
-                  <p  style={{color:'white',textAlign:'left',fontSize:'15px'}}>I am a dedicated and efficient full stack developer in application layers,presentation layers, and database management. I graduated from University of Minnesota Coding Boot Camp and proficient in both front-end and back-end technologies I have developed web applications such as front-end user interface and Back end.</p>
+                  <p  style={{color:'white',textAlign:'left',fontSize:'15px'}}>I am a dedicated and efficient full stack developer in application layers,presentation layers, and database management. I graduated from University of Minnesota Coding Boot Camp and proficient in both front-end and back-end technologies I have developed web applications such as front-end user interface and back end.</p>
                   <p style={{color:'white',textAlign:'left',fontSize:'15px'}}>My objective is to be the creative web developer that i can be and to contribute to the Information Technology industry all that i know and can do.</p>
-                   <p style={{color:'white',textAlign:'left',fontSize:'15px'}}>I have proficiencies in HTML, CSS, JavaScript and skilled with SQL. I also have a solid foundation in MongoDB, Express,React and Node.JS. In addition to work experience in Wells Fargo as a Loan Document Specialist and Freelance Tutor. I hold a Bachelor degree.</p>
+                   <p style={{color:'white',textAlign:'left',fontSize:'15px'}}>I have proficiencies in HTML, CSS, JavaScript and skilled with SQL. I also have a solid foundation in MongoDB, Express,React and Node.JS. In addition to work experience in Wells Fargo as a Loan Document Specialist and Freelance Tutor.</p>
                 
                </Cell>        
            
--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -1,42 +1,44 @@
-import React, { Component } from 'react';
-import {Grid, Cell} from 'react-mdl'
-import Image from '../avani-image1.png'
+import React, { Component } from "react";
+import { Grid, Cell } from "react-mdl";
+import Image from "../avani-image1.png";
 
+class LandingPage extends Component {
+  render() {
+    return (
+      <div style={{ width: "100%", margin: "auto" }}>
+        <Grid className="landing-grid">
+          <Cell col={12}>
+            <img src={Image} alt="avaniImage" className="avtar-image" />
 
-
-
-
-class LandingPage extends Component{
-    render(){
-        return(
-           <div style={{width:'100%',margin:'auto'}}>
-             <Grid className='landing-grid'>
-                 <Cell col={12}>
-                     <img src ={Image} alt = "avaniImage" className = "avtar-image"/>
-                        
-                     <div className = 'banner-text'>
-                         <h1>Avani Dhalgara</h1>
-                         <h1>Full Stack Web Developer</h1>
-                         <hr/>
-                         <p>HTML5/CSS3 | Javascript | React | Bootstrap | MongoDB | MySQL | Node.js | Express</p>
-                         <div className="social-links">
-                             <a href ='https://www.linkedin.com/in/avani-dhalgara/' target ='_blank' rel='noopener noreferrer'>
-                                 <i className="fa fa-linkedin-square"aria-hidden ='true'/>
-                             </a>
-                             <a href ='https://github.com/avanidhalgara' target ='_blank' rel='noopener noreferrer'>
-                                 <i className="fa fa-github-square mr-50"aria-hidden ='true'/>
-                             </a>
-                            
-
-                         </div>
-                     </div>
-                   
-                 </Cell>
-
-             </Grid> 
-           </div> 
-        )
-    }
+            <div className="banner-text">
+              <h1>Avani Dhalgara</h1>
+              <h1>Full Stack Web Developer</h1>
+              <hr />
+              <p>
+                HTML5/CSS3 | Javascript | React | Bootstrap | MongoDB | MySQL |
+                Node.js | Express
+              </p>
+              <div className="social-links">
+                <a
+                  href="https://www.linkedin.com/in/avani-dhalgara/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <i className="fa fa-linkedin-square" aria-hidden="true" />
+                </a>
+                <a
+                  href="https://github.com/avanidhalgara"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <i className="fa fa-github-square mr-50" aria-hidden="true" />
+                </a>
+              </div>
+            </div>
+          </Cell>
+        </Grid>
+      </div>
+    );
+  }
 }
-export default LandingPage
-
+export default LandingPage;
